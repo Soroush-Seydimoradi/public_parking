40c37855c24ebc6a228e171fb5e2cee09bcff689
Soroush <soroushseyedimoradi@gamil.com>
2026-07-09 10:45:52 +0330

    fix: login with phone and username fixed

--- parking_api/auth_serializers.py
+++ parking_api/auth_serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 from .serializers import UserListSerializer
@@ -5,6 +6,12 @@ from .serializers import UserListSerializer
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
+        identifier = attrs.get(self.username_field)
+        if identifier and not User.objects.filter(username=identifier).exists():
+            user = User.objects.filter(profile__phone=identifier).first()
+            if user:
+                attrs[self.username_field] = user.get_username()
+
         data = super().validate(attrs)
         data["user"] = UserListSerializer(self.user).data
         return data
